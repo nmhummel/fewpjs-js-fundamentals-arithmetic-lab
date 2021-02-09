8f341f28411df32f26b64975b915a38b2803ba11
nmhummel <natalie.hummel@gmail.com>
2021-02-08 22:35:14 -0600

    commit tree a3896b1d8aed4fd70c9e35724b6975c722dec1c0

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10) + 1
+
+let num3 = 18
+let num4 = 7
+let mod = num3 % num4
+
+let max = Math.max(1,2,4,6,17,20)
